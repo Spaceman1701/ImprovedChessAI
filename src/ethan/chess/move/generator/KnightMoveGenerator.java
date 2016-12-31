f3b599f7b5bfb9a8270b41290d7a68a3252d8448
Spaceman1701 <fc.spaceman@gmail.com>
2016-12-31 03:23:25 -0700

    DOES NOT COMPILE. more work on refactoring move generation. TODO: refactor MoveGenerator and SetwiseMoveGenerator. KnightMoveGenerator has correct method signitures. TODO: rework Move class to be a class

--- src/ethan/chess/move/generator/KnightMoveGenerator.java
+++ src/ethan/chess/move/generator/KnightMoveGenerator.java
@@ -1,24 +1,65 @@
 package ethan.chess.move.generator;
 
 import ethan.chess.BoardPosition;
+import ethan.chess.SidePosition;
 import ethan.chess.move.Move;
 import ethan.chess.move.SetwiseMoveGenerator;
 import ethan.chess.Side;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Ethan on 12/30/2016.
  */
 public class KnightMoveGenerator implements SetwiseMoveGenerator {
+    private static final int[] KNIGHT_MOVE_SHIFTS = {15, 17, 6, 10}; //for both left and right shifts
 
     @Override
     public long generateMoveBitboard(BoardPosition bp, Side side) {
-        return 0;
+        SidePosition sidePosition = bp.getSidePosition(side);
+        long moves = 0L;
+        long knights = sidePosition.knight;
+        long notSameSide = ~sidePosition.getOccupied();
+        long occupied = bp.getOccupied();
+
+        moves += (knights << KNIGHT_MOVE_SHIFTS[0]) & (~bp.FILE_A) & notSameSide; //15
+        moves += (knights >>> KNIGHT_MOVE_SHIFTS[0]) & (~bp.FILE_H) & notSameSide;
+
+        moves += (knights << KNIGHT_MOVE_SHIFTS[1]) & (~bp.FILE_H) & notSameSide; //17
+        moves += (knights >>> KNIGHT_MOVE_SHIFTS[1]) & (~bp.FILE_A) & notSameSide;
+
+        moves += (knights << KNIGHT_MOVE_SHIFTS[2]) & (~(bp.FILE_H | bp.FILE_G)) & notSameSide; //6
+        moves += (knights >>> KNIGHT_MOVE_SHIFTS[2]) & (~(bp.FILE_A | bp.FILE_B)) & notSameSide;
+
+        moves += (knights << KNIGHT_MOVE_SHIFTS[3]) & (~(bp.FILE_A | bp.FILE_B)) & notSameSide; //10
+        moves += (knights >>> KNIGHT_MOVE_SHIFTS[3]) & (~(bp.FILE_H | bp.FILE_G)) & notSameSide;
+
+        return moves;
     }
 
     @Override
-    public List<Move> generateMoves(long moveBitboard) {
+    public List<Move> generateMoves(long moveBitboard, BoardPosition bp, Side side) {
+        long occupied = bp.getOccupied();
+        long knights = bp.getSidePosition(side).knight;
+        List<Integer> moveList = new ArrayList<>();
+        for (int i = 0; i < BoardPosition.BOARD_SIZE; i++) {
+            if ((moveBitboard & (1L << i)) != 0) {
+                for (int shift : KNIGHT_MOVE_SHIFTS) {
+                    boolean capture = false;
+                    if ((occupied & (1L << (i + shift))) != 0) {
+                        capture = true;
+                    }
+                    if ((knights & (1L << (i + shift))) != 0) {
+                        moveList.add(Move.createMove(side.isWhite(), i + shift, i, capture, false, false)); //TODO: fine captures
+                    }
+                    if ((knights & (1L << (i - shift))) != 0) {
+                        moveList.add(Move.createMove(side.isWhite(), i - shift, i, capture, false, false));
+                    }
+                }
+            }
+        }
+
         return null;
     }
 }
--- src/ethan/chess/move/generator/PawnMoveGenerator.java
+++ src/ethan/chess/move/generator/PawnMoveGenerator.java
@@ -1,10 +1,12 @@
 package ethan.chess.move.generator;
 
 import ethan.chess.BoardPosition;
+import ethan.chess.SidePosition;
 import ethan.chess.move.Move;
 import ethan.chess.move.SetwiseMoveGenerator;
 import ethan.chess.Side;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +15,39 @@ import java.util.List;
 public class PawnMoveGenerator implements SetwiseMoveGenerator {
     @Override
     public long generateMoveBitboard(BoardPosition bp, Side side) {
-        return 0;
+        SidePosition sp = bp.getSidePosition(side);
+        SidePosition op = bp.getOpponentSidePosition(side);
+        long pawns = sp.pawn;
+        long opponentOccupied = op.getOccupied();
+        long fileA = bp.FILE_A;
+        long fileH = bp.FILE_H;
+        long rank8 = bp.RANK_8;
+        long rank2 = bp.RANK_2;
+
+        if (side == Side.BLACK) {
+            pawns = Long.reverse(pawns);
+            opponentOccupied = Long.reverse(opponentOccupied);
+            fileA = Long.reverse(fileA);
+            fileH = Long.reverse(fileH);
+            rank8 = Long.reverse(rank8);
+            rank2 = Long.reverse(rank2);
+        }
+
+
+        long notOccupied = ~op.getOccupied();
+
+
+        long rightAttack = (pawns << 9) & opponentOccupied & (~fileA); //position of pawns shifted 9 where there are black pieces and not on file H
+        long leftAttack = (pawns << 7) & opponentOccupied & (-fileH);
+        long forwardMove = (pawns << 8) & (notOccupied) & (~rank8); //rank 8 would be a promotion TODO: promotions
+        long doubleForwardMove = ((((pawns & rank2) << 8) & notOccupied) << 8) & notOccupied; //yeah, this is confusing... it moves forward one, then two, to ensure there is an empty path
+        //TODO: en passant
+
+        long result = rightAttack | leftAttack | forwardMove | doubleForwardMove;
+        if (side == Side.BLACK) {
+            result = Long.reverse(result);
+        }
+        return result;
     }
 
     @Override
